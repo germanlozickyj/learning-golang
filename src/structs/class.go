2f3662e5e4b51aba62f3029d845e3ba7a100849d
mrgerman <germanlozickyj2003@gmail.com>
2023-05-21 21:22:33 -0300

    recreating constructs in golang

--- src/structs/class.go
+++ src/structs/class.go
@@ -9,12 +9,26 @@ type Employee struct {
 	name string 
 }
 
+func NewEmployee(id int, name string) *Employee {
+	return &Employee{
+		id : id,
+		name : name,
+	}
+}
+
 func main() {
 	e := Employee{}
 	e.SetId(1)
 	e.SetName("Germán")
 	fmt.Println(e.GetId())
 	fmt.Println(e.GetName())
+	employee := Employee{
+		id : 2,
+		name: "Germán 2",
+	}
+	fmt.Println(employee)
+	e4 := NewEmployee(3, "German 3")
+	fmt.Println(*e4)
 }
 
 func (e *Employee) SetId(id int) {
